9de4f7ca9108261265430702a4eb540c7ff188dd
Petros Kapakos <petros.kapakos1@aexp.com>
2017-03-14 23:11:31 -0400

    prepare app for heroku deployment

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"./routing"
+)
+
+func main() {
+
+	router := routing.NewRouter()
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+
+
+
+
+
+
+
